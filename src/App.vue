bb66219b5a36fde37b675941bf4edfd5ff044005
fajri-isnanto <fajriisnanto1011@gmail.com>
2024-06-13 11:01:19 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <DataFetcher />
+  </div>
+</template>
+
+<script>
+import DataFetcher from './components/DataFetcher.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DataFetcher,
+  },
+};
+</script>
+
+<style>
+/* Add global styles here */
+</style>
---
+++ src/components/DataFetcher.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <h1>Data from External API</h1>
+    <div v-if="error">{{ error.message }}</div>
+    <pre v-else-if="data">{{ data }}</pre>
+    <div v-else>Loading...</div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        data: null,
+        error: null,
+      };
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+        try {
+          const response = await axios.get('http://service-user:80');
+          this.data = response.data;
+        } catch (error) {
+          this.error = error;
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add component-specific styles here */
+  </style>
+  
